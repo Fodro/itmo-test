49e6e2c39d4b1017a5b65c71c34708c0824fd713
fodro <feodor.tomilov@yandex.ru>
2022-11-26 13:44:39 +0300

    feat: connected to db
    * installed Doctrine
    * generated Entities
    * generated Migrations

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -4,6 +4,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 #[Route('/author', name: 'author_')]
 class AuthorController extends AbstractController {
 	#[Route('/all', name: "all")]
